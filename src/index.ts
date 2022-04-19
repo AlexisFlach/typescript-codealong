713e6bd38969ba6110e2af7d30bc149700b817e0
Alexis Flach <alexis.flach87@gmail.com>
2022-04-19 13:13:06 +0200

    add something

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express, { Request, Response } from 'express';
+import { User } from './models/User';
+import { users } from './db';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello my world');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.post('/', (req: Request, res: Response) => {
+  console.log(req.body);
 });
 
 const PORT = 4001;
