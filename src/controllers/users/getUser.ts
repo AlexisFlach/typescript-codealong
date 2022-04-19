f68167e79ed7b807a629e904dd9c55568616a75b
Alexis Flach <alexis.flach87@gmail.com>
2022-04-19 15:21:39 +0200

    add try catch

--- src/controllers/users/getUser.ts
+++ src/controllers/users/getUser.ts
@@ -2,10 +2,11 @@ import { Request, Response } from 'express';
 import { users } from '../../db';
 
 export const getUser = (req: Request, res: Response) => {
-  const user = users.find((u) => u.id === parseInt(req.params.id));
-  if (!user) {
-    res.status(404).send('User not found');
-  } else {
-    res.send(user);
+  try {
+    const user = users.find((banan) => banan.id === parseInt(req.params.id));
+    if (!user) res.status(404).send('User not found');
+    res.status(200).send(user);
+  } catch (e: any) {
+    res.status(500).send(e.message);
   }
 };
